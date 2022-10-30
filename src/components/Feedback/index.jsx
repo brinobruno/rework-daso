0eedc17a0f7695c6312698299ea9547b497f7f9e
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-10-30 08:20:02 -0300

    ongoing: Progress on feedbacks slider

--- src/components/Feedback/index.jsx
+++ src/components/Feedback/index.jsx
@@ -1,3 +1,14 @@
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/swiper-bundle.min.css'
+import SwiperCore, { EffectFlip, Navigation, Pagination } from 'swiper'
+
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+import 'swiper/css/grid'
+import 'swiper/css/autoplay'
+
+
 import { useGetFeedbackContent } from './useGetFeedbackContent'
 
 export const Feedback = () => {
@@ -13,21 +24,40 @@ export const Feedback = () => {
     >
       <div className='max-w-6xl w-full flex items-center justify-center
       text-center'>
-        <h3 className='font-poppins font-bold text-4xl pb-4 text-center'>
-          Depoimentos
-        </h3>
-      </div>
+        <div>
+          <h3 className='font-poppins font-bold text-4xl pb-4 text-center'>
+            Depoimentos
+          </h3>
 
-      <div>
-        {data.feedbacks.map((item => (
-          <>
-          <span>{ item.reviewerName }</span>
-          <span>{ item.commentary }</span>
-          <img src={ item.reviewerPicture.url } alt="" className='max-w-[100px]'/>
-          </>
-          
-        )))}
+          <div className=''>
+            <Swiper
+              pagination={{ clickable: true }}
+              spaceBetween={0}
+              slidesPerView={1}
+              mousewheel={true}
+              className='mySwiper'
+              modules={[Pagination]}
+            >
+              {data.feedbacks.map(((item, index) => (
+                <SwiperSlide className='w-full'>
+                  <div className='max-w-[300px]'>
+                    <span>{item.reviewerName}</span>
+                    <span>{item.commentary}</span>
+                    <img
+                      src={item.reviewerPicture.url}
+                      alt=''
+                      className='max-w-[100px]'
+                    />
+                  </div>
+                </SwiperSlide>
+              )))}
+            </Swiper>
+
+
       </div>
-    </section>
+    </div>
+      </div >
+
+    </section >
   )
 }
