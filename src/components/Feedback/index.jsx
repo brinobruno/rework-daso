42ee2960e7443d636925119664865f8d55090bbc
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-10-31 20:21:51 -0300

    feat: Feedback section working properly

--- src/components/Feedback/index.jsx
+++ src/components/Feedback/index.jsx
@@ -1,13 +1,13 @@
 import { Swiper, SwiperSlide } from 'swiper/react'
-import SwiperCore, { Pagination, Mousewheel } from 'swiper'
+import SwiperCore, { Pagination, Mousewheel, Autoplay } from 'swiper'
+import { useGetFeedbackContent } from './useGetFeedbackContent'
+
+import userIcon from './../../assets/user.png'
 
 import 'swiper/css'
 import 'swiper/css/pagination'
 import 'swiper/swiper.min.css'
 import 'swiper/swiper-bundle.min.css'
-
-
-import { useGetFeedbackContent } from './useGetFeedbackContent'
 
 export const Feedback = () => {
   const { data, loading, error } = useGetFeedbackContent()
@@ -15,7 +15,7 @@ export const Feedback = () => {
   if (loading) return <span>Loading...</span>
   if (error) return <span>Server error!</span>
 
-  SwiperCore.use([Mousewheel])
+  SwiperCore.use([Mousewheel, Autoplay])
 
   return (
     <section
@@ -37,6 +37,11 @@ export const Feedback = () => {
           pagination={{ clickable: true }}
           slidesPerView={ 1 }
           mousewheel
+          loop
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: true
+          }}
           breakpoints={{
             768: {
               slidesPerView: 2,
@@ -46,7 +51,9 @@ export const Feedback = () => {
         >
           {data.feedbacks.map(((item, index) => (
             <SwiperSlide key={ index }>
-              <blockquote className='w-full p-8 rounded-[0.25rem]'>
+              <blockquote className='w-full p-8 rounded-[0.25rem] bg-white
+              max-h-[240px] h-[240px] flex justify-between flex-col
+              cursor-pointer'>
                 <p className='mb-6 indent-7 font-josefin text-lg text-gray-900
                 relative'>
                   <span className='font-bold text-purple-500 absolute
@@ -56,19 +63,23 @@ export const Feedback = () => {
                     { item.commentary }
                 </p>
 
-                <cite className='flex items-center font-normal'>
+                <cite className='flex items-end'>
                   <img
-                    src={ item.reviewerPicture.url }
-                    alt=''
+                    src={
+                      item?.reviewerPicture?.url
+                      ? item.reviewerPicture.url
+                      : userIcon
+                    }
+                    alt={ item?.reviewerName ? item.reviewerName : 'Anônimo' }
                     className='w-[2rem] h-[2rem] object-cover clip mr-2'
                     loading='lazy'
                   />
-                  <span>
-                    { item.reviewerName }
+                  <span className='font-josefin text-lg text-gray-900
+                  not-italic'>
+                    { item?.reviewerName ? item.reviewerName : 'Anônimo' }
                   </span>
                 </cite>
               </blockquote>
-
             </SwiperSlide>
           )))}
         </Swiper>
