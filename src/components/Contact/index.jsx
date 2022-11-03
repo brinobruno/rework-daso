a308fb9d9e36dbadad502cb87faab3a5cfcdb1c2
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-11-03 20:41:23 -0300

    ONGOING: Progress on form styling and markup

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -40,18 +40,75 @@ export const Contact = () => {
           </div>
         </div>
 
-        <div>
+        <div className='flex bg-purple-100 justify-center items-center
+        w-full max-w-[600px] mx-auto p-10 rounded-xl'>
           <form
             ref={ form }
             onSubmit={ sendEmail }
+            className='flex justify-between items-start flex-col'
           >
-            <label>Name</label>
-            <input type="text" name="user_name" />
-            <label>Email</label>
-            <input type="email" name="user_email" />
-            <label>Message</label>
-            <textarea name="message" />
-            <button type="submit" value="Send" />
+            <div className='flex justify-between items-start gap-10'>
+              <div className='flex flex-col'>
+                <label
+                  className='font-josefin text-lg text-gray-900'
+                >
+                  Name*
+                </label>
+                <input
+                  type="text"
+                  name="user_name"
+                  placeholder='Nome'
+                  required
+                  className='mb-[2.4rem] py-2 px-4 rounded-md'
+                />
+
+                <label
+                  className='font-josefin text-lg text-gray-900'
+                >
+                  Email*
+                </label>
+                <input
+                  type="email"
+                  name="user_email"
+                  placeholder='Email'
+                  required
+                  className='mb-9 py-2 px-4 rounded-md'
+                />  
+
+                <label
+                  className='font-josefin text-lg text-gray-900'
+                >
+                  WhatsApp
+                </label>
+                <input
+                  type="tel"
+                  name="user_email"
+                  placeholder='Email'
+                  className='mb-8 py-2 px-4 rounded-md'
+                />
+              </div>
+
+              <div className='flex flex-col'>
+                <label
+                  className='font-josefin text-lg text-gray-900'
+                >
+                  Assunto*
+                </label>
+                <textarea
+                  name="message"
+                  placeholder='Mensagem'
+                  required
+                  className='resize-none pt-2 pb-[100%] px-4 rounded-md'
+                />
+              </div>
+            </div>
+
+            <button
+              type="submit"
+              value="Send"
+            >
+              Enviar
+            </button>
           </form>
         </div>
       </div>
