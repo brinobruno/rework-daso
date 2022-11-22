c1b94fe9682158fb66c856f916d4e22a225dd4d4
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-11-21 21:06:58 -0300

    feat: Add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+export const Footer = () => {
+  return (
+    <footer
+      className='flex justify-center py-32 bg-[#a56ab9] text-[#FAFAFA]
+      flex-col'
+    >
+      <div className='w-full max-w-6xl mx-auto'>
+        <div className='w-full flex items-center justify-between text-center'>
+          <div>
+            aa
+          </div>
+          <div>
+            aa
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
